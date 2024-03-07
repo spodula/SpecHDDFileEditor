f8c19099320d68fae508b002afa39bae5f3ab57c
spodula <graham@salkin.co.uk>
2024-03-07 22:57:16 +0000

    Corrected "JUMPTO" block

--- src/hddEditor/libs/disks/LINEAR/tzxblocks/JumpToBlock.java
+++ src/hddEditor/libs/disks/LINEAR/tzxblocks/JumpToBlock.java
@@ -9,11 +9,11 @@ public class JumpToBlock extends TZXBlock {
 	public int Disp;
 	
 	public JumpToBlock(RandomAccessFile fs) throws IOException {
-		blocktype = TZX.TZX_LOOPSTART;
-		BlockDesc = "Loop start";
+		blocktype = TZX.TZX_JUMP;
+		BlockDesc = "Jump to";
 		byte dat[] = new byte[2];
 		fs.read(dat);
-		Disp = ((int)dat[0] & 0xff) + (((int)dat[1] & 0xff) * 0x100);
+		Disp = GetDblByte(dat, 0);
 		
 		if (Disp > 0x8000) {
 			Disp = Disp - 0x10000;
