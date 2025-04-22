c66e814be9f06b9d518d2d72f29cb2ae0e4f9432
graham <graham@graham-linux>
2025-04-22 20:40:50 +0100

    Fix. Removed ThisEntry as its overriding the parent objects version.

--- src/hddEditor/ui/partitionPages/dialogs/edit/TrDosFileEditDialog.java
+++ src/hddEditor/ui/partitionPages/dialogs/edit/TrDosFileEditDialog.java
@@ -1,4 +1,5 @@
-package hddEditor.ui.partitionPages.dialogs.edit; 
+package hddEditor.ui.partitionPages.dialogs.edit;
+
 //TODO: SCL files just dont seem to work.
 /**
  * Implementation of the Edit file page for a TR-DOS file.
@@ -32,17 +33,13 @@ import hddEditor.ui.partitionPages.FileRenderers.CodeRenderer;
 import hddEditor.ui.partitionPages.FileRenderers.NumericArrayRenderer;
 
 public class TrDosFileEditDialog extends EditFileDialog {
-	// Directory entry of the file being displayed
-	//private TrdDirectoryEntry ThisEntry = null;
-	private FileEntry ThisEntry = null;
-
 	/**
 	 * Constructor
 	 * 
 	 * @param display
 	 */
-	public TrDosFileEditDialog(Display display, FileSelectDialog filesel,IDEDosPartition CurrentPartition) {
-		super(display, filesel,CurrentPartition);
+	public TrDosFileEditDialog(Display display, FileSelectDialog filesel, IDEDosPartition CurrentPartition) {
+		super(display, filesel, CurrentPartition);
 	}
 
 	/**
@@ -76,22 +73,22 @@ public class TrDosFileEditDialog extends EditFileDialog {
 		gd = new GridData(GridData.FILL_BOTH);
 		gd.horizontalSpan = 4;
 		MainPage1.setLayoutData(gd);
-		
+
 		MainPage = new Composite(MainPage1, SWT.NONE);
 		MainPage1.setContent(MainPage);
-		
+
 		gridLayout = new GridLayout();
 		gridLayout.numColumns = 4;
 		gridLayout.makeColumnsEqualWidth = true;
 		MainPage.setLayout(gridLayout);
-		
+
 		MainPage1.addControlListener(new ControlListener() {
-			
+
 			@Override
 			public void controlResized(ControlEvent arg0) {
 				MainPage1.setMinSize(MainPage.computeSize(MainPage1.getClientArea().width, SWT.DEFAULT));
 			}
-			
+
 			@Override
 			public void controlMoved(ControlEvent arg0) {
 			}
@@ -107,18 +104,18 @@ public class TrDosFileEditDialog extends EditFileDialog {
 	 */
 	private void RenderAppropriatePage() {
 		try {
-			TrdDirectoryEntry trde = (TrdDirectoryEntry)ThisEntry; 
+			TrdDirectoryEntry trde = (TrdDirectoryEntry) ThisEntry;
 			char ftype = trde.GetFileType();
 			if (ftype == 'B') {
 				BasicRenderer CurrentRenderer = new BasicRenderer();
-				CurrentRenderer.RenderBasic(MainPage, data, null, ThisEntry.GetFilename(), ThisEntry.GetFileSize(), 
+				CurrentRenderer.RenderBasic(MainPage, data, null, ThisEntry.GetFilename(), ThisEntry.GetFileSize(),
 						trde.GetVar2(), trde.startline, filesel, null);
 			} else if (ftype != 'D') {
 				CodeRenderer CurrentRenderer = new CodeRenderer();
-				//TODO: implement TRDosFileEdit.saveevent for CODE
+				// TODO: implement TRDosFileEdit.saveevent for CODE
 
-				CurrentRenderer.RenderCode(MainPage, data, null, ThisEntry.GetFilename(), data.length,
-						trde.GetVar1(), filesel,CurrentPartition,null);
+				CurrentRenderer.RenderCode(MainPage, data, null, ThisEntry.GetFilename(), data.length, trde.GetVar1(),
+						filesel, CurrentPartition, null);
 			} else if (trde.IsCharArray()) {
 				CharArrayRenderer CurrentRenderer = new CharArrayRenderer();
 				CurrentRenderer.RenderCharArray(MainPage, data, null, ThisEntry.GetFilename(), "A", filesel, null);
